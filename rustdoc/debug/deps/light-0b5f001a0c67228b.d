29fdbcf7b0a639a1d0ff78bbf0240b16cc0c0c27
germolinal <germolinal@users.noreply.github.com>
2023-03-13 06:40:18 +0000

    Deploying to gh-pages from @ SIMPLE-BuildingSimulation/light@c7e954020abda29477fe586c95aae362df845b6a 🚀

--- rustdoc/debug/deps/light-0b5f001a0c67228b.d
+++
@@ -1,8 +0,0 @@
-/home/runner/work/light/light/./docs/rustdoc/debug/deps/light-0b5f001a0c67228b.rmeta: src/lib.rs src/model.rs src/optical_info.rs src/solar_surface.rs
-
-/home/runner/work/light/light/./docs/rustdoc/debug/deps/light-0b5f001a0c67228b.d: src/lib.rs src/model.rs src/optical_info.rs src/solar_surface.rs
-
-src/lib.rs:
-src/model.rs:
-src/optical_info.rs:
-src/solar_surface.rs:
---
+++ rustdoc/debug/deps/light-1a7ddf69dfc7d972.d
@@ -0,0 +1,8 @@
+/home/runner/work/light/light/./docs/rustdoc/debug/deps/light-1a7ddf69dfc7d972.rmeta: src/lib.rs src/model.rs src/optical_info.rs src/solar_surface.rs
+
+/home/runner/work/light/light/./docs/rustdoc/debug/deps/light-1a7ddf69dfc7d972.d: src/lib.rs src/model.rs src/optical_info.rs src/solar_surface.rs
+
+src/lib.rs:
+src/model.rs:
+src/optical_info.rs:
+src/solar_surface.rs:
